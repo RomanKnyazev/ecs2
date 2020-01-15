3deb206dae971c3ae4bf0784f31f7f75c1bfc3a8
Leopotam <leopotam@gmail.com>
2020-01-15 12:25:20 +0300

    * Init.

---
+++ src/EcsComponent.cs
@@ -0,0 +1,107 @@
+// ----------------------------------------------------------------------------
+// The MIT License
+// Simple Entity Component System framework https://github.com/Leopotam/ecs
+// Copyright (c) 2017-2020 Leopotam <leopotam@gmail.com>
+// ----------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+// ReSharper disable ClassNeverInstantiated.Global
+
+namespace Leopotam.Ecs2 {
+    /// <summary>
+    /// Marks component type to be not auto-filled as GetX in filter.
+    /// </summary>
+    public interface IEcsIgnoreInFilter { }
+
+    /// <summary>
+    /// Marks field of IEcsSystem class to be ignored during dependency injection.
+    /// </summary>
+    public sealed class EcsIgnoreInjectAttribute : Attribute { }
+
+    /// <summary>
+    /// Global descriptor of used component type.
+    /// </summary>
+    /// <typeparam name="T">Component type.</typeparam>
+    public static class EcsComponentType<T> where T : struct {
+        // ReSharper disable StaticMemberInGenericType
+        public static readonly int TypeIndex;
+        public static readonly Type Type;
+        public static readonly bool IsIgnoreInFilter;
+        // ReSharper restore StaticMemberInGenericType
+
+        static EcsComponentType () {
+            TypeIndex = Interlocked.Increment (ref EcsComponentPool.ComponentTypesCount);
+            Type = typeof (T);
+            IsIgnoreInFilter = typeof (IEcsIgnoreInFilter).IsAssignableFrom (Type);
+        }
+    }
+
+    public sealed class EcsComponentPool {
+        /// <summary>
+        /// Global component type counter.
+        /// First component will be "1" for correct filters updating (add component on positive and remove on negative).
+        /// </summary>
+        internal static int ComponentTypesCount;
+    }
+
+    public interface IEcsComponentPool {
+        void Recycle (int idx);
+    }
+
+#if ENABLE_IL2CPP
+    [Unity.IL2CPP.CompilerServices.Il2CppSetOption (Unity.IL2CPP.CompilerServices.Option.NullChecks, false)]
+    [Unity.IL2CPP.CompilerServices.Il2CppSetOption (Unity.IL2CPP.CompilerServices.Option.ArrayBoundsChecks, false)]
+#endif
+    public sealed class EcsComponentPool<T> : IEcsComponentPool where T : struct {
+        public T[] Items = new T[128];
+        int[] _reservedItems = new int[128];
+        int _itemsCount;
+        int _reservedItemsCount;
+        internal static T Default;
+
+        internal EcsComponentPool () { }
+
+        /// <summary>
+        /// Sets new capacity (if more than current amount).
+        /// </summary>
+        /// <param name="capacity">New value.</param>
+        public void SetCapacity (int capacity) {
+            if (capacity > Items.Length) {
+                Array.Resize (ref Items, capacity);
+            }
+        }
+
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
+        public int New () {
+            int id;
+            if (_reservedItemsCount > 0) {
+                id = _reservedItems[--_reservedItemsCount];
+            } else {
+                id = _itemsCount;
+                if (_itemsCount == Items.Length) {
+                    Array.Resize (ref Items, _itemsCount << 1);
+                }
+                _itemsCount++;
+            }
+            return id;
+        }
+
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
+        public ref T GetItem (int idx) {
+            return ref Items[idx];
+        }
+
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
+        public void Recycle (int idx) {
+            Items[idx] = default;
+            if (_reservedItemsCount == _reservedItems.Length) {
+                Array.Resize (ref _reservedItems, _reservedItemsCount << 1);
+            }
+            _reservedItems[_reservedItemsCount++] = idx;
+        }
+    }
+}
