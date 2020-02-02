2d36ba5f0f150fa6009aa9ebcf432c7cbe8d16a5
Leopotam <leopotam@gmail.com>
2020-02-02 13:37:17 +0300

    * EcsFilter: Versions for 3 and 4 components added.

--- src/EcsFilter.cs
+++ src/EcsFilter.cs
@@ -330,8 +330,9 @@ namespace Leopotam.Ecs2 {
         public override void OnAddEntity (in EcsEntity entity) {
             if (AddDelayedOp (true, entity)) { return; }
             if (Entities.Length == EntitiesCount) {
-                Array.Resize (ref Entities, EntitiesCount << 1);
-                if (_allow1) { Array.Resize (ref _Get1, EntitiesCount << 1); }
+                var newSize = EntitiesCount << 1;
+                Array.Resize (ref Entities, newSize);
+                if (_allow1) { Array.Resize (ref _Get1, newSize); }
             }
             // inlined and optimized World.GetComponent() call.
             ref var entityData = ref entity.Owner.GetEntityData (entity);
@@ -440,9 +441,10 @@ namespace Leopotam.Ecs2 {
         public override void OnAddEntity (in EcsEntity entity) {
             if (AddDelayedOp (true, entity)) { return; }
             if (Entities.Length == EntitiesCount) {
-                Array.Resize (ref Entities, EntitiesCount << 1);
-                if (_allow1) { Array.Resize (ref _Get1, EntitiesCount << 1); }
-                if (_allow2) { Array.Resize (ref _Get2, EntitiesCount << 1); }
+                var newSize = EntitiesCount << 1;
+                Array.Resize (ref Entities, newSize);
+                if (_allow1) { Array.Resize (ref _Get1, newSize); }
+                if (_allow2) { Array.Resize (ref _Get2, newSize); }
             }
             // inlined and optimized World.GetComponent() call.
             ref var entityData = ref entity.Owner.GetEntityData (entity);
@@ -486,14 +488,319 @@ namespace Leopotam.Ecs2 {
             }
         }
 
-        public class Exclude<Exc1> : EcsFilter<Inc1> where Exc1 : struct {
+        public class Exclude<Exc1> : EcsFilter<Inc1, Inc2> where Exc1 : struct {
             protected Exclude (EcsWorld world) : base (world) {
                 ExcludedTypeIndices = new[] { EcsComponentType<Exc1>.TypeIndex };
                 ExcludedTypes = new[] { EcsComponentType<Exc1>.Type };
             }
         }
 
-        public class Exclude<Exc1, Exc2> : EcsFilter<Inc1> where Exc1 : struct where Exc2 : struct {
+        public class Exclude<Exc1, Exc2> : EcsFilter<Inc1, Inc2> where Exc1 : struct where Exc2 : struct {
+            protected Exclude (EcsWorld world) : base (world) {
+                ExcludedTypeIndices = new[] { EcsComponentType<Exc1>.TypeIndex, EcsComponentType<Exc2>.TypeIndex };
+                ExcludedTypes = new[] { EcsComponentType<Exc1>.Type, EcsComponentType<Exc2>.Type };
+            }
+        }
+    }
+
+#if ENABLE_IL2CPP
+    [Unity.IL2CPP.CompilerServices.Il2CppSetOption (Unity.IL2CPP.CompilerServices.Option.NullChecks, false)]
+    [Unity.IL2CPP.CompilerServices.Il2CppSetOption (Unity.IL2CPP.CompilerServices.Option.ArrayBoundsChecks, false)]
+#endif
+#if UNITY_2019_1_OR_NEWER
+    [UnityEngine.Scripting.Preserve]
+#endif
+    public class EcsFilter<Inc1, Inc2, Inc3> : EcsFilter where Inc1 : struct where Inc2 : struct where Inc3 : struct {
+        int[] _Get1;
+        int[] _Get2;
+        int[] _Get3;
+        readonly bool _allow1;
+        readonly bool _allow2;
+        readonly bool _allow3;
+
+        readonly EcsComponentPool<Inc1> _pool1;
+        readonly EcsComponentPool<Inc2> _pool2;
+        readonly EcsComponentPool<Inc3> _pool3;
+
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
+        public ref Inc1 Get1 (int idx) {
+            return ref _pool1.Items[_Get1[idx]];
+        }
+
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
+        public ref Inc2 Get2 (int idx) {
+            return ref _pool2.Items[_Get2[idx]];
+        }
+
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
+        public ref Inc3 Get3 (int idx) {
+            return ref _pool3.Items[_Get3[idx]];
+        }
+
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
+        public EcsComponentRef<Inc1> Get1Ref (int idx) {
+            return _pool1.Ref (_Get1[idx]);
+        }
+
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
+        public EcsComponentRef<Inc2> Get2Ref (int idx) {
+            return _pool2.Ref (_Get2[idx]);
+        }
+
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
+        public EcsComponentRef<Inc3> Get3Ref (int idx) {
+            return _pool3.Ref (_Get3[idx]);
+        }
+
+        protected EcsFilter (EcsWorld world) {
+            _allow1 = !EcsComponentType<Inc1>.IsIgnoreInFilter;
+            _allow2 = !EcsComponentType<Inc2>.IsIgnoreInFilter;
+            _allow3 = !EcsComponentType<Inc3>.IsIgnoreInFilter;
+            _pool1 = world.GetPool<Inc1> ();
+            _pool2 = world.GetPool<Inc2> ();
+            _pool3 = world.GetPool<Inc3> ();
+            _Get1 = _allow1 ? new int[EcsHelpers.FilterEntitiesSize] : null;
+            _Get2 = _allow2 ? new int[EcsHelpers.FilterEntitiesSize] : null;
+            _Get3 = _allow3 ? new int[EcsHelpers.FilterEntitiesSize] : null;
+            IncludedTypeIndices = new[] { EcsComponentType<Inc1>.TypeIndex, EcsComponentType<Inc2>.TypeIndex, EcsComponentType<Inc3>.TypeIndex };
+            IncludedTypes = new[] { EcsComponentType<Inc1>.Type, EcsComponentType<Inc2>.Type, EcsComponentType<Inc3>.Type };
+        }
+
+        /// <summary>
+        /// Event for adding compatible entity to filter.
+        /// Warning: Don't call manually!
+        /// </summary>
+        /// <param name="entity">Entity.</param>
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
+        public override void OnAddEntity (in EcsEntity entity) {
+            if (AddDelayedOp (true, entity)) { return; }
+            if (Entities.Length == EntitiesCount) {
+                var newSize = EntitiesCount << 1;
+                Array.Resize (ref Entities, newSize);
+                if (_allow1) { Array.Resize (ref _Get1, newSize); }
+                if (_allow2) { Array.Resize (ref _Get2, newSize); }
+                if (_allow3) { Array.Resize (ref _Get3, newSize); }
+            }
+            // inlined and optimized World.GetComponent() call.
+            ref var entityData = ref entity.Owner.GetEntityData (entity);
+            var allow1 = _allow1;
+            var allow2 = _allow2;
+            var allow3 = _allow3;
+            for (int i = 0, iMax = entityData.ComponentsCountX2; i < iMax; i += 2) {
+                var typeIdx = entityData.Components[i];
+                var itemIdx = entityData.Components[i + 1];
+                if (allow1 && typeIdx == EcsComponentType<Inc1>.TypeIndex) {
+                    _Get1[EntitiesCount] = itemIdx;
+                    allow1 = false;
+                }
+                if (allow2 && typeIdx == EcsComponentType<Inc2>.TypeIndex) {
+                    _Get2[EntitiesCount] = itemIdx;
+                    allow2 = false;
+                }
+                if (allow3 && typeIdx == EcsComponentType<Inc3>.TypeIndex) {
+                    _Get3[EntitiesCount] = itemIdx;
+                    allow3 = false;
+                }
+            }
+            Entities[EntitiesCount++] = entity;
+            ProcessListeners (true, entity);
+        }
+
+        /// <summary>
+        /// Event for removing non-compatible entity to filter.
+        /// Warning: Don't call manually!
+        /// </summary>
+        /// <param name="entity">Entity.</param>
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
+        public override void OnRemoveEntity (in EcsEntity entity) {
+            if (AddDelayedOp (false, entity)) { return; }
+            for (int i = 0, iMax = EntitiesCount; i < iMax; i++) {
+                if (Entities[i] == entity) {
+                    EntitiesCount--;
+                    if (i < EntitiesCount) {
+                        Entities[i] = Entities[EntitiesCount];
+                        if (_allow1) { _Get1[i] = _Get1[EntitiesCount]; }
+                        if (_allow2) { _Get2[i] = _Get2[EntitiesCount]; }
+                        if (_allow3) { _Get3[i] = _Get3[EntitiesCount]; }
+                    }
+                    ProcessListeners (false, entity);
+                    break;
+                }
+            }
+        }
+
+        public class Exclude<Exc1> : EcsFilter<Inc1, Inc2, Inc3> where Exc1 : struct {
+            protected Exclude (EcsWorld world) : base (world) {
+                ExcludedTypeIndices = new[] { EcsComponentType<Exc1>.TypeIndex };
+                ExcludedTypes = new[] { EcsComponentType<Exc1>.Type };
+            }
+        }
+
+        public class Exclude<Exc1, Exc2> : EcsFilter<Inc1, Inc2, Inc3> where Exc1 : struct where Exc2 : struct {
+            protected Exclude (EcsWorld world) : base (world) {
+                ExcludedTypeIndices = new[] { EcsComponentType<Exc1>.TypeIndex, EcsComponentType<Exc2>.TypeIndex };
+                ExcludedTypes = new[] { EcsComponentType<Exc1>.Type, EcsComponentType<Exc2>.Type };
+            }
+        }
+    }
+
+#if ENABLE_IL2CPP
+    [Unity.IL2CPP.CompilerServices.Il2CppSetOption (Unity.IL2CPP.CompilerServices.Option.NullChecks, false)]
+    [Unity.IL2CPP.CompilerServices.Il2CppSetOption (Unity.IL2CPP.CompilerServices.Option.ArrayBoundsChecks, false)]
+#endif
+#if UNITY_2019_1_OR_NEWER
+    [UnityEngine.Scripting.Preserve]
+#endif
+    public class EcsFilter<Inc1, Inc2, Inc3, Inc4> : EcsFilter where Inc1 : struct where Inc2 : struct where Inc3 : struct where Inc4 : struct {
+        int[] _Get1;
+        int[] _Get2;
+        int[] _Get3;
+        int[] _Get4;
+        readonly bool _allow1;
+        readonly bool _allow2;
+        readonly bool _allow3;
+        readonly bool _allow4;
+
+        readonly EcsComponentPool<Inc1> _pool1;
+        readonly EcsComponentPool<Inc2> _pool2;
+        readonly EcsComponentPool<Inc3> _pool3;
+        readonly EcsComponentPool<Inc4> _pool4;
+
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
+        public ref Inc1 Get1 (int idx) {
+            return ref _pool1.Items[_Get1[idx]];
+        }
+
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
+        public ref Inc2 Get2 (int idx) {
+            return ref _pool2.Items[_Get2[idx]];
+        }
+
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
+        public ref Inc3 Get3 (int idx) {
+            return ref _pool3.Items[_Get3[idx]];
+        }
+
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
+        public ref Inc4 Get4 (int idx) {
+            return ref _pool4.Items[_Get4[idx]];
+        }
+
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
+        public EcsComponentRef<Inc1> Get1Ref (int idx) {
+            return _pool1.Ref (_Get1[idx]);
+        }
+
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
+        public EcsComponentRef<Inc2> Get2Ref (int idx) {
+            return _pool2.Ref (_Get2[idx]);
+        }
+
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
+        public EcsComponentRef<Inc3> Get3Ref (int idx) {
+            return _pool3.Ref (_Get3[idx]);
+        }
+
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
+        public EcsComponentRef<Inc4> Get4Ref (int idx) {
+            return _pool4.Ref (_Get4[idx]);
+        }
+
+        protected EcsFilter (EcsWorld world) {
+            _allow1 = !EcsComponentType<Inc1>.IsIgnoreInFilter;
+            _allow2 = !EcsComponentType<Inc2>.IsIgnoreInFilter;
+            _allow3 = !EcsComponentType<Inc3>.IsIgnoreInFilter;
+            _allow4 = !EcsComponentType<Inc4>.IsIgnoreInFilter;
+            _pool1 = world.GetPool<Inc1> ();
+            _pool2 = world.GetPool<Inc2> ();
+            _pool3 = world.GetPool<Inc3> ();
+            _pool4 = world.GetPool<Inc4> ();
+            _Get1 = _allow1 ? new int[EcsHelpers.FilterEntitiesSize] : null;
+            _Get2 = _allow2 ? new int[EcsHelpers.FilterEntitiesSize] : null;
+            _Get3 = _allow3 ? new int[EcsHelpers.FilterEntitiesSize] : null;
+            _Get4 = _allow4 ? new int[EcsHelpers.FilterEntitiesSize] : null;
+            IncludedTypeIndices = new[] { EcsComponentType<Inc1>.TypeIndex, EcsComponentType<Inc2>.TypeIndex, EcsComponentType<Inc3>.TypeIndex, EcsComponentType<Inc4>.TypeIndex };
+            IncludedTypes = new[] { EcsComponentType<Inc1>.Type, EcsComponentType<Inc2>.Type, EcsComponentType<Inc3>.Type, EcsComponentType<Inc4>.Type };
+        }
+
+        /// <summary>
+        /// Event for adding compatible entity to filter.
+        /// Warning: Don't call manually!
+        /// </summary>
+        /// <param name="entity">Entity.</param>
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
+        public override void OnAddEntity (in EcsEntity entity) {
+            if (AddDelayedOp (true, entity)) { return; }
+            if (Entities.Length == EntitiesCount) {
+                var newSize = EntitiesCount << 1;
+                Array.Resize (ref Entities, newSize);
+                if (_allow1) { Array.Resize (ref _Get1, newSize); }
+                if (_allow2) { Array.Resize (ref _Get2, newSize); }
+                if (_allow3) { Array.Resize (ref _Get3, newSize); }
+                if (_allow4) { Array.Resize (ref _Get4, newSize); }
+            }
+            // inlined and optimized World.GetComponent() call.
+            ref var entityData = ref entity.Owner.GetEntityData (entity);
+            var allow1 = _allow1;
+            var allow2 = _allow2;
+            var allow3 = _allow3;
+            var allow4 = _allow4;
+            for (int i = 0, iMax = entityData.ComponentsCountX2; i < iMax; i += 2) {
+                var typeIdx = entityData.Components[i];
+                var itemIdx = entityData.Components[i + 1];
+                if (allow1 && typeIdx == EcsComponentType<Inc1>.TypeIndex) {
+                    _Get1[EntitiesCount] = itemIdx;
+                    allow1 = false;
+                }
+                if (allow2 && typeIdx == EcsComponentType<Inc2>.TypeIndex) {
+                    _Get2[EntitiesCount] = itemIdx;
+                    allow2 = false;
+                }
+                if (allow3 && typeIdx == EcsComponentType<Inc3>.TypeIndex) {
+                    _Get3[EntitiesCount] = itemIdx;
+                    allow3 = false;
+                }
+                if (allow4 && typeIdx == EcsComponentType<Inc3>.TypeIndex) {
+                    _Get4[EntitiesCount] = itemIdx;
+                    allow4 = false;
+                }
+            }
+            Entities[EntitiesCount++] = entity;
+            ProcessListeners (true, entity);
+        }
+
+        /// <summary>
+        /// Event for removing non-compatible entity to filter.
+        /// Warning: Don't call manually!
+        /// </summary>
+        /// <param name="entity">Entity.</param>
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
+        public override void OnRemoveEntity (in EcsEntity entity) {
+            if (AddDelayedOp (false, entity)) { return; }
+            for (int i = 0, iMax = EntitiesCount; i < iMax; i++) {
+                if (Entities[i] == entity) {
+                    EntitiesCount--;
+                    if (i < EntitiesCount) {
+                        Entities[i] = Entities[EntitiesCount];
+                        if (_allow1) { _Get1[i] = _Get1[EntitiesCount]; }
+                        if (_allow2) { _Get2[i] = _Get2[EntitiesCount]; }
+                        if (_allow3) { _Get3[i] = _Get3[EntitiesCount]; }
+                        if (_allow4) { _Get4[i] = _Get4[EntitiesCount]; }
+                    }
+                    ProcessListeners (false, entity);
+                    break;
+                }
+            }
+        }
+
+        public class Exclude<Exc1> : EcsFilter<Inc1, Inc2, Inc3, Inc4> where Exc1 : struct {
+            protected Exclude (EcsWorld world) : base (world) {
+                ExcludedTypeIndices = new[] { EcsComponentType<Exc1>.TypeIndex };
+                ExcludedTypes = new[] { EcsComponentType<Exc1>.Type };
+            }
+        }
+
+        public class Exclude<Exc1, Exc2> : EcsFilter<Inc1, Inc2, Inc3, Inc4> where Exc1 : struct where Exc2 : struct {
             protected Exclude (EcsWorld world) : base (world) {
                 ExcludedTypeIndices = new[] { EcsComponentType<Exc1>.TypeIndex, EcsComponentType<Exc2>.TypeIndex };
                 ExcludedTypes = new[] { EcsComponentType<Exc1>.Type, EcsComponentType<Exc2>.Type };
